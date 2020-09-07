e37b63ad0d84de097bf4586a156f475419591ff1
Alexandr Moroz <felplenz1998@gmail.com>
2020-09-07 21:23:29 +0300

    master, string, lesson3

---
+++ src/string.java
@@ -0,0 +1,11 @@
+public class string {
+    public static void main(String [] args){
+    int x = 5;
+    String s = "Hello";
+    String space = " ";
+    String name = "Alex";
+    System.out.println(s+space+name);
+    System.out.println("Hello" + " " + "User");
+    System.out.println("My" + space + "number" + space + x);
+    }
+}
