f8aed5a45eed3293d4c69b54d56395fc181d2bc2
Alexandr Moroz <felplenz1998@gmail.com>
2020-09-07 21:12:28 +0300

    Master, variables, Lesson-2

---
+++ src/variables.java
@@ -0,0 +1,22 @@
+public class variables {
+    public static void main(String [] args){
+        int myInt = 357;
+        short myShort = 3266;
+        long myLong = 21456754;
+
+        double myDouble = 234.43;
+        float myFloat = 432.4f;
+
+        char c = 'a';
+        boolean b = true;
+
+        byte myByte = 100; //-128 . 128
+        System.out.println(myInt);
+        System.out.println(myShort);
+        System.out.println(myLong);
+        System.out.println(myDouble);
+        System.out.println(myFloat);
+        System.out.println(c);
+        System.out.println(b);
+    }
+}
