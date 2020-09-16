491fa220aa64f7c16348441a56e702799ba4fa8e
Alexandr Moroz <felplenz1998@gmail.com>
2020-09-16 06:02:56 +0300

    If else if else

---
+++ src/If.java
@@ -0,0 +1,12 @@
+public class If {
+    public static void main(String[] args) {
+        int myInt = 50;
+        if (myInt < 10) {
+            System.out.println("Verno");
+        } else if (myInt > 30) {
+            System.out.println("Ne Verno");
+        } else {
+            System.out.println("vse ostal`noe");
+        }
+    }
+}
