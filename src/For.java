e0ed0e697edc65f00cd6a6552aba28e282eca087
Alexandr Moroz <felplenz1998@gmail.com>
2020-09-16 05:51:17 +0300

    cycle For

---
+++ src/For.java
@@ -0,0 +1,10 @@
+public class For {
+    public static void main(String[] args) {
+        for(int i = 0; i<=10; i++){
+            System.out.println("Hello" + i);
+        }
+        for(int i = 10; i>=0; i--){
+            System.out.println("Hello" + i);
+        }
+    }
+}
