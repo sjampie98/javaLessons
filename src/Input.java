2b605edacf8f1fb35bd15e43c074fbb73441a31e
Alexandr Moroz <felplenz1998@gmail.com>
2020-09-17 19:52:11 +0300

    Input, scanner

---
+++ src/Input.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("VVedite 4to nibut`");
+        String string = s.nextLine();
+        System.out.println(("vu vveli " +string));
+    }
+}
